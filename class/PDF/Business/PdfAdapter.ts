642155fc5a2299cd00108b5eb9300e39451670b5
PikkMoune71 <antoinepicard385@gmail.com>
2023-06-20 10:51:07 +0200

    refactor PdfAdapter

--- class/PDF/Business/PdfAdapter.ts
+++ class/PDF/Business/PdfAdapter.ts
@@ -7,68 +7,71 @@ import { ElementPdfTxt } from '../ElementPdfTxt';
 export class PdfAdapter {
   constructor() {}
 
-  getElementsForPdf(
-    width: number,
-    height: number
-  ): AbstractElementPdf[] {
+  getElementsForPdf(width: number, height: number): AbstractElementPdf[] {
     const dataRadar: ExtractRadarIncidents = new ExtractRadarIncidents();
     const data: Map<String, FlashRadar[]> = dataRadar.groupFlashRadarByDate();
 
-    let datasForPdf: AbstractElementPdf[] = [];
-    const title: ElementPdfTxt = new ElementPdfTxt(
-      width / 2 - 60,
-      height - 4 * 50,
-      'Incidents',
-      30,
-      '#000000'
-    );
-    datasForPdf.push(title);
-
-    const logo: ElementPdfImage = new ElementPdfImage(
-      width - 196,
-      height - 110,
-      'https://franceactive-occitanie.org/wp-content/uploads/2021/01/logo-etat-francais.png',
-      0.5
+    const datasForPdf: AbstractElementPdf[] = [];
+    datasForPdf.push(
+      new ElementPdfTxt(
+        width / 2 - 60,
+        height - 4 * 50,
+        'Incidents',
+        30,
+        '#000000'
+      )
     );
 
-    datasForPdf.push(logo);
+    datasForPdf.push(
+      new ElementPdfImage(
+        width - 196,
+        height - 110,
+        'https://franceactive-occitanie.org/wp-content/uploads/2021/01/logo-etat-francais.png',
+        0.5
+      )
+    );
+
     let nbIncidentsTotal: number = 0;
 
-    // affiche les plaques d'immatriculation par date
     let i: number = 0;
     data.forEach((value: FlashRadar[], key: String) => {
       const nbFlashRadar = value.length;
       nbIncidentsTotal += nbFlashRadar;
-      const element: ElementPdfTxt = new ElementPdfTxt(
-        50,
-        height - 4 * 70 - i * 50,
-        key.toString() + ' - ' + nbFlashRadar + ' Incidents',
-        20,
-        '#000000'
-      );
-      datasForPdf.push(element);
-      i++;
-      value.forEach((flashRadar: FlashRadar) => {
-        const element: ElementPdfTxt = new ElementPdfTxt(
+      datasForPdf.push(
+        new ElementPdfTxt(
           50,
           height - 4 * 70 - i * 50,
-          flashRadar.getLicense(),
-          15,
+          key.toString() + ' - ' + nbFlashRadar + ' Incidents',
+          20,
           '#000000'
+        )
+      );
+      i++;
+
+      value.forEach((flashRadar: FlashRadar) => {
+        datasForPdf.push(
+          new ElementPdfTxt(
+            50,
+            height - 4 * 70 - i * 50,
+            flashRadar.getLicense(),
+            15,
+            '#000000'
+          )
         );
-        datasForPdf.push(element);
         i++;
       });
     });
 
-    const element: ElementPdfTxt = new ElementPdfTxt(
-      width / 2 - 120,
-      height - 4 * 70 - i * 50,
-      "Nombre d'incidents total : " + nbIncidentsTotal,
-      20,
-      '#000000'
+    datasForPdf.push(
+      new ElementPdfTxt(
+        width / 2 - 120,
+        height - 4 * 70 - i * 50,
+        "Nombre d'incidents total : " + nbIncidentsTotal,
+        20,
+        '#000000'
+      )
     );
-    datasForPdf.push(element);
+
     return datasForPdf;
   }
 }
